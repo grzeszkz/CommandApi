218d332cac075249c1d75ed3e91340788083a6b6
Grzegorz L <grzeszkz@gmail.com>
2023-08-29 12:04:06 +0200

    Added Command Repository Mock Data

---
+++ src/CommandApi/Data/ICommandAPIRepo.cs
@@ -0,0 +1,15 @@
+﻿using CommandApi.Models;
+using System.Collections.Generic;
+
+namespace CommandApi.Data
+{
+    public interface ICommandAPIRepo
+    {
+        bool SaveChanges();
+        IEnumerable<Command> GetAllCommands();
+        Command GetCommandById(int id);
+        void CreateCommand(Command cmd);
+        void DeleteCommand(Command cmd);
+        void UpdateCommand(Command cmd);
+    }
+}
