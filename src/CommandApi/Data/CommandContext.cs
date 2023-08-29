f585a3fedfa3f6adff3148d92d6b3f47774d71ac
Grzegorz L <grzeszkz@gmail.com>
2023-08-29 13:09:31 +0200

    Added DBContext

---
+++ src/CommandApi/Data/CommandContext.cs
@@ -0,0 +1,14 @@
+﻿using CommandApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CommandApi.Data
+{
+    public class CommandContext : DbContext
+    {
+        public CommandContext(DbContextOptions<CommandContext> options)
+        : base(options)
+        {
+        }
+        public DbSet<Command> CommandItems { get; set; }
+    }
+}
