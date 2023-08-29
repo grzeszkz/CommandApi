b5f7a82487189df76e895798a41d91ea3c26ef5a
Grzegorz L <grzeszkz@gmail.com>
2023-08-29 21:51:49 +0200

    Added new form of db connection

--- src/CommandApi/Startup.cs
+++ src/CommandApi/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.EntityFrameworkCore;
+using Npgsql;
 
 namespace CommandApi
 {
@@ -24,7 +25,12 @@ namespace CommandApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<CommandContext>(opt => opt.UseNpgsql(Configuration.GetConnectionString("PostgreSqlConnection")));
+            var builder = new NpgsqlConnectionStringBuilder();
+            builder.ConnectionString = Configuration.GetConnectionString("PostgreSqlConnection");
+            builder.Username = Configuration["UserID"];
+            builder.Password = Configuration["Password"];
+
+            services.AddDbContext<CommandContext>(opt => opt.UseNpgsql(builder.ConnectionString));
             //SECTION 1. Add code below
             services.AddControllers();
 
